dbc3eb2b8096a3773be99ccfbbc636a905ce5925
gerashdo <gerardo.jimenezarguelles@gmail.com>
2023-01-17 15:47:00 -0600

    Configure TopTabsBar

--- src/navigators/TopTabsNavigator.tsx
+++ src/navigators/TopTabsNavigator.tsx
@@ -3,15 +3,46 @@ import { createMaterialTopTabNavigator } from '@react-navigation/material-top-ta
 import { ChatsScreen } from '../screens/ChatsScreen';
 import { ContactsScreen } from '../screens/ContactsScreen';
 import { AlbumsScreen } from '../screens/AlbumsScreen';
+import { colors } from '../theme/appThema';
+import { Text } from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 export const TopTabsNavigator = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="ChatsScreen" component={ ChatsScreen } />
-      <Tab.Screen name="ContactsScreen" component={ ContactsScreen } />
-      <Tab.Screen name="AlbumsScreen" component={ AlbumsScreen } />
+    <Tab.Navigator
+        sceneContainerStyle={{ backgroundColor: 'white'}}
+        screenOptions={ ({ route }) => ({
+            tabBarShowIcon: true,
+            tabBarPressColor: colors.primary,
+            tabBarIndicatorStyle: {
+                backgroundColor: colors.primary
+            },
+            tabBarStyle: {
+                elevation: 0,
+                shadowColor: 'transparent'
+            },
+            tabBarIcon: ({ focused, color }) => {
+                let iconName = ''
+                switch( route.name ){
+                    case 'ChatsScreen':
+                        iconName='Ch'
+                        break
+                    case 'ContactsScreen':
+                        iconName='Co'
+                        break
+                    case 'AlbumsScreen':
+                        iconName='Al'
+                        break
+                }
+
+                return <Text style={{ color }}>{ iconName }</Text>
+            }
+        })}
+    >
+      <Tab.Screen name="ChatsScreen" options={{ tabBarLabel: 'Chats' }} component={ ChatsScreen } />
+      <Tab.Screen name="ContactsScreen" options={{ tabBarLabel: 'Contactos' }} component={ ContactsScreen } />
+      <Tab.Screen name="AlbumsScreen" options={{ tabBarLabel: 'Albums' }} component={ AlbumsScreen } />
     </Tab.Navigator>
   );
 }
